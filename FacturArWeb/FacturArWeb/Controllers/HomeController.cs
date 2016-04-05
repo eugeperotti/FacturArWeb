64f37b86ef6d9a58ac9dc33ee1bc85cfdaf7f593
Maria Eugenia Perotti <eugenia.perotti@netkel.com>
2016-04-05 17:22:50 -0300

    Hola Mundo

--- FacturArWeb/FacturArWeb/Controllers/HomeController.cs
+++ FacturArWeb/FacturArWeb/Controllers/HomeController.cs
@@ -8,22 +8,13 @@ namespace FacturArWeb.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Hola()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
