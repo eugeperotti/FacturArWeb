dbcd4581d64144eec9b64c435994802b7fdf1a7d
Maria Eugenia Perotti <eugenia.perotti@netkel.com>
2016-04-05 10:59:05 -0300

    Inicial - MVC e Individual Authentication

---
+++ FacturArWeb/FacturArWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FacturArWeb.Startup))]
+namespace FacturArWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
